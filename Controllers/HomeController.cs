e8608ed6d03d397cb583ab88f975cb221be75680
Ahmed Sabbour <sabbour@outlook.com>
2017-09-29 02:43:26 +0400

    Updated queue name

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -66,7 +66,7 @@ namespace customerfeedback_web.Controllers
             );
 
              var queueClient = storageAccount.CreateCloudQueueClient();
-             var messageQueue = queueClient.GetQueueReference("messageQueue");
+             var messageQueue = queueClient.GetQueueReference("feedbackqueue");
              await messageQueue.CreateIfNotExistsAsync();
 
              var feedbackDetails = new {
